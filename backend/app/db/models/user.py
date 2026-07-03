392d5593a98b7ecba4250d1bee72be90aa709dc3
Gabe Adelsberg <gabeadelsberg@gmail.com>
2026-07-03 19:34:03 -0400

    feat: add modular user API route

--- backend/app/db/models/user.py
+++ backend/app/db/models/user.py
@@ -7,5 +7,9 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True, nullable=False)
-    email = Column(String, unique=True, index=True, nullable=False)
+
+    username = Column(String, unique=True, nullable=False)
+
+    email = Column(String, unique=True, nullable=False)
+
+    hashed_password = Column(String, nullable=False)
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from app.api import users
 from app.core.config import settings
 from app.db.database import Base, engine
 from app.db import models
@@ -10,7 +10,7 @@ app = FastAPI(
     title=settings.app_name,
     version="0.1.0"
 )
-
+app.include_router(users.router)
 
 @app.get("/")
 def root():
